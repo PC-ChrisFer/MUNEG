891c38bce9921bd9a42cec60b679f12a774221ce
PC-ChrisFer <86115295+PC-ChrisFer@users.noreply.github.com>
2022-06-15 17:13:56 -0700

    Agregar modelos y apis en php y las contantes, functions y conexiones

---
+++ api/modelos/reporte.php
@@ -0,0 +1,168 @@
+<?php
+//Maneja la tabla de categoria  de la base de datos
+//Contiene validaciones de validator
+
+class reporte extends validator
+{
+    //Declaración de atributos (propiedades)
+    private $id_reporte = null;
+    private $asunto = null;
+    private $descripcion = null;
+    private $inquilino_id = null;
+    private $estado = null;
+
+    //Variables true -- false
+    private $true = true;
+    private $false = '0';
+
+    //Metodos para setear los valores de los campos
+    //Id - serial
+    public function setId($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->id_reporte = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Asunto - Varchar
+    public function setAsunto($value)
+    {
+        if ($this->validateAlphabetic($value, 1, 100)) {
+            $this->asunto = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Descripcion - varchar
+    public function setDescripcion($value)
+    {
+        if ($this->validateAlphabetic($value, 6, 300)) {
+            $this->descripcion = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Id Inquilino - Integer
+    public function setInquilino($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->inquilino_id = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Id Inquilino - Integer
+    public function setEstado($value)
+    {
+        if ($this->validateBoolean($value)) {
+            $this->inquilino_id = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    //Metodos para obtener los valores de los campos
+
+    //Id - Serial
+    public function getId()
+    {
+        return $this->id_reporte;
+    }
+
+    //Nombre de Usuario - Varchar
+    public function getAsunto()
+    {
+        return $this->asunto;
+    }
+
+    //Password - Varchar
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    //Tipo Usuario - Integer
+    public function getInquilino()
+    {
+        return $this->inquilino_id;
+    }
+
+    //Estado - Boolean
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    //Metodos para realizar las operaciones SCRUD(Search, Create, Read, Update, Delete)
+
+    //Metodo para la busqueda SEARCH
+    //(nombre_tipo, nombre_usuario, nombre_propietario)
+    public function searchRows($value)
+    {
+        $sql = 'SELECT id_reporte, asunto, descripcion, estado ,reporte.id_inquilino, nombre
+        FROM public.reporte
+        INNER JOIN public.inquilino
+        ON inquilino.id_inquilino = reporte.id_inquilino
+        WHERE nombre ILIKE ?';
+        $params = array("%$value%");
+        return Database::getRows($sql, $params);
+    }
+
+    //Metodo para la busqueda READALL
+    //(sin parametros)
+    public function readAll()
+    {
+        $sql = 'SELECT id_reporte, asunto, descripcion, estado,reporte.id_inquilino, nombre
+        FROM public.reporte
+        INNER JOIN public.inquilino
+        ON inquilino.id_inquilino = reporte.id_inquilino';
+        $params = null;
+        return Database::getRows($sql, $params);
+    }
+
+
+    //Metodo para la insercción INSERT (propietario)
+    //(nombre_usuario, password, tipo_usuario, propietario)
+    public function createRow()
+    {
+        $sql = 'INSERT INTO public.reporte(
+            asunto, descripcion, id_inquilino, estado)
+            VALUES (?, ?, ?, ?)';
+        $params = array($this->asunto, $this->descripcion, $this->inquilino_id, $this->true);
+        return Database::executeRow($sql, $params);
+    }
+
+
+    //Metodo para la actualización UPDATE
+    //(nombre_usuario, password, tipo_usuario, propietario, usuario)
+    public function updateRow()
+    {
+        $sql = 'UPDATE public.reporte
+        SET asunto=?, descripcion=?, id_inquilino=?, estado=?
+        WHERE id_reporte=?';
+        $params = array($this->asunto, $this->descripcion, $this->inquilino_id, $this->estado, $this->id_reporte);
+        return Database::executeRow($sql, $params);
+    }
+
+    //Metodo para la eliminación DELETE
+    //(visibilidad, id_tipo_propietario)
+    public function deleteRow()
+    {
+        $sql = 'UPDATE public.reporte
+        SET estado=?
+        WHERE id_reporte=?';
+        $params = array($this->false, $this->id_reporte);
+        return Database::executeRow($sql, $params);
+    }
+}
---
+++ controller/components.js
@@ -0,0 +1,110 @@
+//@ts-check
+//Importar las constantes y metodos de components.js y api_constant.js
+import { APIConnection } from "./APIConnection.js";
+import {
+  API_READALL,
+  API_SEARCH,
+  API_SUCESS_REQUEST,
+  GET_METHOD,
+  POST_METHOD,
+  API_DELETE,
+  SERVER,
+  API_READONE,
+  API_UNDELETE,
+} from "./constants/api_constant.js";
+import { getElementById } from "./constants/functions.js";
+
+// LEER REGISTROS
+export async function readRows(ENDPOINT, fillrows) {
+  let APIEndpoint = ENDPOINT + API_READALL;
+  //Llamar a la función de conexión api para realizar fetch y then
+  let APIResponse = await APIConnection(APIEndpoint, GET_METHOD, null);
+  if (APIResponse.status == API_SUCESS_REQUEST) {
+    fillrows(APIResponse.dataset)
+    return
+  }
+  // dado caso este if se ejecute con "return" hara que hasta este punto llegue el codigo
+}
+
+// BUSCAR REGISTROS
+export async function searchRows(ENDPOINT, formID, fillrows, parametersJson) {
+  let APIEndpoint = ENDPOINT + API_SEARCH;
+  //@ts-ignore
+  let parameters = formID ? new FormData(getElementById(formID)) : parametersJson;
+
+  //Llamar a la función de conexión api para realizar fetch y then
+  let APIResponse = await APIConnection(APIEndpoint, POST_METHOD, parameters);
+  //Utilizar la respuesta del api para realizar funciones
+  if (APIResponse.status == API_SUCESS_REQUEST) {
+    fillrows(APIResponse.dataset)
+    // dado caso este if se ejecute con "return" hara que hasta este punto llegue el codigo
+    return;
+  } else {
+    fillrows([])
+    // dado caso este if se ejecute con "return" hara que hasta este punto llegue el codigo
+    return;
+  }
+}
+
+
+
+
+// GUARDAR REGISTROS
+export async function saveRow(ENDPOINT, ACTION, parameters, fillrows) {
+  // ingresando valores a variables
+  let APIEndpoint = ENDPOINT + ACTION;
+
+  // ejecutando request hacia la API
+  let APIResponse = await APIConnection(APIEndpoint, POST_METHOD, parameters);
+  // validando respuesta 
+  if (APIResponse.status == API_SUCESS_REQUEST) {
+    fillrows(APIResponse.dataset)
+    //$('#guardado').modal('show');
+    return;
+  }
+  //En caso de fracaso se abrira un modal de error
+  //$('#error_proceso').modal('show');
+
+}
+
+// ELIMINAR REGISTROS
+export async function deleteRow(ENDPOINT, parameters, fillrows) {
+  let APIEndpoint = ENDPOINT + API_DELETE;
+
+  let APIResponse = await APIConnection(APIEndpoint, POST_METHOD, parameters);
+
+  if (APIResponse.status == API_SUCESS_REQUEST) {
+    fillrows(APIResponse.dataset)
+    //$('#eliminado').modal('show');
+    return;
+  }
+  //En caso de fracaso se abrira un modal de error
+  //$('#error_proceso').modal('show');
+}
+
+// Hacer un readOne
+export async function readOne(ENDPOINT, parameters, fillrows) {
+  let APIEndpoint = ENDPOINT + API_READONE;
+
+  let APIResponse = await APIConnection(APIEndpoint, POST_METHOD, parameters);
+
+  console.log(APIResponse);  
+  if (APIResponse.status == API_SUCESS_REQUEST) {
+    fillrows(APIResponse.dataset)
+    return;
+  }
+}
+
+// ELIMINAR REGISTROS
+export async function unDeleteRow(ENDPOINT, parameters, fillrows) {
+  let APIEndpoint = ENDPOINT + API_UNDELETE;
+
+  let APIResponse = await APIConnection(APIEndpoint, POST_METHOD, parameters);
+
+  if (APIResponse.status == API_SUCESS_REQUEST) {
+    fillrows(APIResponse.dataset)
+    return;
+  }
+  //En caso de fracaso se abrira un modal de error
+  //$('#error_proceso').modal('show');
+}

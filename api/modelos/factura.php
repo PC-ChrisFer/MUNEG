80f0ecd2253e6de807a7c6830cf76209af288801
emanuelL8 <emanuel012300@gmail.com>
2022-06-19 13:18:08 -0600

    PHP terminados

--- api/modelos/factura.php
+++ api/modelos/factura.php
@@ -54,7 +54,7 @@ class factura extends validator
         return true;
     }
     //Venta Gravada
-    public function setId($value)
+    public function setVenta($value)
     {
         $this->venta_gravada = $value;
         return true;
@@ -128,5 +128,66 @@ class factura extends validator
         return $this->id_inquilino;
         
     }
+
+    //Metodos para realizar las operaciones SCRUD(Search, Create, Read, Update, Delete)
+
+    //Metodo para la busqueda SEARCH
+    //Utilizaremos los campos o (NOMBRE_TIPO)
+    public function searchRows($value)
+    {
+        $sql = 'SELECT id_factura, codigo_factura, descripcion, direccion, subtotal, "IVA", venta_gravada, fecha, id_inquilino
+        FROM public.factura
+        WHERE codigo_factura = ? ';
+        $params = array("%$value%");
+        return Database::getRows($sql, $params);
+    }
+
+    //Metodo para la inserción INSERT
+    public function createRow()
+    {
+        $sql = 'INSERT INTO public.factura(
+        codigo_factura, descripcion, direccion, subtotal, "IVA", venta_gravada, fecha, id_inquilino)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+        $params = array($this->codigo_factura, $this->descripcion, $this->direccion, $this->subtotal, $this->IVA, $this->venta_gravada, $this->fecha, $this->id_inquilino);
+        return Database::executeRow($sql, $params);
+    }
     
+    //Metodo para la actualización UPDATE
+    public function updateRow()
+    {
+        $sql = 'UPDATE public.factura
+        SET codigo_factura=?, descripcion=?, direccion=?, subtotal=?, "IVA"=?, venta_gravada=?, fecha=?, id_inquilino=?
+        WHERE id_factura = ?';
+        $params = array($this->codigo_factura, $this->descripcion, $this->direccion, $this->subtotal, $this->IVA, $this->venta_gravada, $this->fecha, $this->id_inquilino);
+        return Database::executeRow($sql, $params);
+    }
+
+    //Metodo para la eliminación DELETE 
+    public function deleteRow()
+    {
+        $sql = 'DELETE FROM public.factura
+	    WHERE id_factura';
+        $params = array($this->id_factura);
+        return Database::executeRow($sql, $params);
+    }
+
+    //Metodo para leer READ
+    //Leer todas las filas de la Tabla
+    public function readAll()
+    {
+        $sql = 'SELECT id_factura, codigo_factura, descripcion, direccion, subtotal, "IVA", venta_gravada, fecha, id_inquilino
+        FROM public.factura';
+        $params = null;
+        return Database::getRows($sql, $params);
+    }
+
+    //Leer solamente una fila de la Tabla
+    public function readOne()
+    {
+        $sql = 'SELECT id_factura, codigo_factura, descripcion, direccion, subtotal, "IVA", venta_gravada, fecha, id_inquilino
+        FROM public.factura
+        WHERE id_factura = ?';
+        $params = ($this->id_factura);
+        return Database::getRow($sql, $params);
+    }
 }
